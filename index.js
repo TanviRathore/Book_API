9b238c53364e11c047c46549ec0ebf4ee2e1abd3
Tanvi Rathore <tanvirathore1607@gmail.com>
2021-08-28 21:49:13 +0530

    Added Microservices

---
+++ index.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+
+// importing API
+const Book = require("./API/book");
+const Author = require("./API/author");
+const Publication = require("./API/publication");
+
+// connecting mongoose
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+    })
+    .then(() => console.log("connection extablished!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+    //initiallizing express
+    const ourApp = express();
+
+    // every data we send or retrieve is in json format, therefore specifying that we are using JSON
+    ourApp.use(express.json());
+
+    // Microservices
+    ourApp.use("/book",Book);
+    ourApp.use("/author",Author);
+    ourApp.use("/publication",Publication);
+
+    OurAPP.get("/", (request, response) => {
+        response.json({ message: "Server is working!!!!!!" });
+    });
+    
+    // listening to server
+    OurAPP.listen(4000, () => console.log("Server is running"));
+
