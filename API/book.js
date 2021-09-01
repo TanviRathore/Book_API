324f531baeef1d1d40adb67b0ddaa04be95031d8
Tanvi Rathore <tanvirathore1607@gmail.com>
2021-09-01 16:32:42 +0530

    put request added

--- API/book.js
+++ API/book.js
@@ -8,4 +8,54 @@ Router.get("/book",async (req, res) => {
     return res.json(getAllBooks);
 });
 
+Router.get("/book/:bookID",async (req, res) => {
+    const getBookByID = await BookModel.findOne({
+        ISBN: req.params.bookID,
+    });
+
+    if(!getBookByID)
+    {
+        return res.json({
+            error: `No book found for ISBN of ${req.params.bookID}`,
+        });
+    }
+    
+    return res.json({
+        book: getBookByID,
+    });
+});
+
+Router.get("/book/:category",async (req, res) => {
+    const getBookByCategory = await BookModel.findOne({
+        ISBN: req.params.category,
+    });
+
+    if(!getBookByCategory)
+    {
+        return res.json({
+            error: `No book found for ISBN of ${req.params.category}`,
+        });
+    }
+    
+    return res.json({
+        book: getBookByCategory,
+    });
+});
+
+Router.post("/book/new",async (req, res) => {
+    try
+    {
+        const {newBook} = req.params.body;
+
+        await BookModel.create(newBook);
+        return res.json({
+            message: `Book added successfully`
+        });
+    }
+    catch(error) {
+        return res.json({ error: error.message });
+    }
+});
+
+
 module.exports = Router;
