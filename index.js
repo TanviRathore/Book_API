c49b71313c6658411515e6c2846fcb3f15507bd2
Tanvi Rathore <tanvirathore1607@gmail.com>
2021-09-01 16:14:18 +0530

    connected MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const express = require("express");
 
 //initiallizing express
@@ -11,13 +11,7 @@ const Author = require("./API/author");
 const Publication = require("./API/publication");
 
 // connecting mongoose
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-    })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("connection extablished!"))
     .catch((err) => {
         console.log(err);
